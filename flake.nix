f3cd821166f48edb99920d10fe07edb3ca33f746
Sidharth N. Babu <sidharth.n.babu@gmail.com>
2025-11-05 19:42:20 -0500

    flake files

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nova Hackathon frontend dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_20
+            pkgs.pnpm
+            pkgs.git
+          ];
+
+          shellHook = ''
+            export PATH=$PWD/node_modules/.bin:$PATH
+            export NEXT_TELEMETRY_DISABLED=1
+          '';
+        };
+      });
+}
